6a5ffede55d2105c25f995bf978edaa5a3e84b58
bridennis <5626164+bridennis@users.noreply.github.com>
2019-03-30 21:59:02 +0300

    добавлен модульный тест с мокированием через фасад

--- tests/Unit/ExchangeServiceTest.php
+++ tests/Unit/ExchangeServiceTest.php
@@ -2,13 +2,33 @@
 
 namespace Tests\Unit;
 
+use App\Repository\Exchange\FcmExchangeRateRepository;
 use App\Services\ExchangeService;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class ExchangeServiceTest extends TestCase
 {
+
+    /**
+     * @var string ответ от сервиса FCM
+     */
+    public static $fcmCacheGet = <<<'EOD'
+<?xml version="1.0" encoding="utf-8"?>
+<ValCurs Date="28.03.2019" name="Foreign Currency Market">
+    <Valute ID="R01235">
+        <NumCode>840</NumCode>
+        <CharCode>USD</CharCode>
+        <Nominal>1</Nominal>
+        <Name>Доллар США</Name>
+        <Value>64,5925</Value>
+    </Valute>
+</ValCurs>
+EOD;
+
     /**
      * Конвертируем 100 USD в рубли.
+     * Используем внешний класс для мокирования.
      *
      * @return void
      */
@@ -23,5 +43,34 @@ class ExchangeServiceTest extends TestCase
         $exchangeService = new ExchangeService($mockFcmExchangeRateRepository);
 
         $this->assertEquals($expected, $exchangeService->convertFromCurrencyToRub($amount, $currency));
+    }
+
+    /**
+     * Конвертируем 100 USD в рубли.
+     * Используем мокирование фасадов.
+     *
+     * @return void
+     */
+    public function testMockingCacheFacadeAndConvertFromUSDToRub()
+    {
+
+        $amount = 100;
+        $currency = 'USD';
+        $expected = '6459.25';
+
+        Cache::shouldReceive('has')
+            ->twice()
+            ->with(config('exchange.currency_market.cache.key'))
+            ->andReturn(true);
+
+        Cache::shouldReceive('get')
+            ->twice()
+            ->with(config('exchange.currency_market.cache.key'))
+            ->andReturn(static::$fcmCacheGet);
+
+        $exchangeService = new ExchangeService(new FcmExchangeRateRepository(''));
+
+        $this->assertEquals($expected, $exchangeService->convertFromCurrencyToRub($amount, $currency));
+
     }
 }
