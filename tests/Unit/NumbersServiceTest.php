54480dad49f6ec2cd6bd95b5500a388caac2f553
bridennis <5626164+bridennis@users.noreply.github.com>
2019-03-31 14:44:40 +0300

    использование DI контейнера для мокирования анонимным классом

--- tests/Unit/NumbersServiceTest.php
+++ tests/Unit/NumbersServiceTest.php
@@ -2,21 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Services\NumbersService;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * Тестируем пример вывода.
-     *
-     * @return void
-     */
-    public function testExpectedOutputIsTrue()
-    {
-        $fileContents = '43534 кот 45 23';
-        $digit = '4';
-
-        $expectedString = <<<'EOD'
+    public static $fileContents = '43534 кот 45 23';
+    public static $digit = '4';
+    public static $expectedString = <<<'EOD'
 [
 43534,
 45
@@ -24,10 +17,53 @@ class ExampleTest extends TestCase
 
 EOD;
 
-        $mockNumbersService = new MockNumbersService($fileContents);
+    /**
+     * Тестируем пример вывода.
+     *
+     * @return void
+     */
+    public function testExpectedOutputIsTrue()
+    {
+        $mockNumbersService = new MockNumbersService(static::$fileContents);
 
-        $this->expectOutputString($expectedString);
+        $this->expectOutputString(static::$expectedString);
 
-        echo $mockNumbersService::getFromFileByDigit('', $digit);
+        echo $mockNumbersService::getFromFileByDigit('', static::$digit);
+    }
+
+    /**
+     * Тестируем пример вывода.
+     * Используем контейнер внедрения зависимостей и мокирование анонимным классом.
+     *
+     * @return void
+     */
+    public function testMockingThroughAnonymousClassNumbersServiceExpectedOutputIsTrue()
+    {
+        $fileContents = static::$fileContents;
+        app()->bind('App\Services\NumbersService', function () use ($fileContents) {
+            return new class($fileContents) extends NumbersService
+            {
+                private static $fileContents;
+
+                public function __construct($fileContents)
+                {
+                    static::$fileContents = $fileContents;
+                }
+
+                protected static function validateArgsGetFromFileByDigitArgs(string $file, int $digit): bool
+                {
+                    return true;
+                }
+
+                protected static function genStringsFromFile(string $fileName)
+                {
+                    yield static::$fileContents;
+                }
+            };
+        });
+
+        $this->expectOutputString(static::$expectedString);
+
+        echo app('App\Services\NumbersService')::getFromFileByDigit('', static::$digit);
     }
 }
